eecf415cc6860f42db1255955bb0d56c774911c9
Jon-Magnus Rosenblad <jonmagnusr@gmail.com>
2022-07-28 14:21:32 +0200

    Add output for size of permuted set

--- graph_automorphisms.R
+++ graph_automorphisms.R
@@ -7,6 +7,10 @@ for (filename in args) {
     g.dot <- read.dot(filename)
     g.dot <- g.dot + t(g.dot)
     g.graph <- graph.adjacency(g.dot)
+    print(filename)
     print(automorphisms(g.graph))
-    print(automorphism_group(g.graph))
+    auts = automorphism_group(g.graph)
+    for (aut in auts) {
+        cat("Permuted indices =",sum(aut != 1:length(aut)),"/",length(aut),"\n")
+    }
 }
